9a2813635c4c20d25c0eea4db70d505c77c4a2f4
Uelsuam3 <87491444+Uelsuam3@users.noreply.github.com>
2022-05-10 15:37:20 +0200

    Content view
    Have the Orange Background

---
+++ ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  Marmelade_Test
+//
+//  Created by I.E. on 10/05/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Color(.orange)
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
